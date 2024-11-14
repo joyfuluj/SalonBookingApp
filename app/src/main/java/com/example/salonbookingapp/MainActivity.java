60192152947ee870f607eab370a2186a17f5eb62
joyfuluj <joyumejiego@yahoo.com>
2024-11-14 12:40:27 -0800

    Connected Database

--- app/src/main/java/com/example/salonbookingapp/MainActivity.java
+++ app/src/main/java/com/example/salonbookingapp/MainActivity.java
@@ -8,17 +8,23 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class MainActivity extends AppCompatActivity {
+    private DatabaseReference root;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        root = FirebaseDatabase.getInstance().getReference();
+//        root.setValue("HelloWorld");
+    }
+
+    protected void onButton1Pressed(Bundle savedInstanceState){
+        root.setValue("HelloWorld");
     }
 }
