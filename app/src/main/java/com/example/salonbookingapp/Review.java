c3d2486cee3b31dee03d9c94cba1e0a5075c0b7b
joyfuluj <joyumejiego@yahoo.com>
2024-11-23 01:51:37 -0800

    add salon detail function and review

--- app/src/main/java/com/example/salonbookingapp/Review.java
+++ app/src/main/java/com/example/salonbookingapp/Review.java
@@ -1,6 +1,8 @@
 package com.example.salonbookingapp;
 
 import android.os.Bundle;
+import android.widget.RatingBar;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +10,57 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class Review extends AppCompatActivity {
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
+public class Review extends AppCompatActivity {
+    String salonName;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_review);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        salonName = getIntent().getStringExtra("salonName");
+
+        TextView salon = findViewById(R.id.textView7);
+        salon.setText(salonName);
+        TextView overall1 = findViewById(R.id.textView6);
+        TextView overall2 = findViewById(R.id.textView5);
+        RatingBar customerRating = findViewById(R.id.ratingBar6);
+
+        TextView allCusService = findViewById(R.id.textView8);
+        TextView allPrice = findViewById(R.id.textView9);
+        TextView allserviceQuality = findViewById(R.id.textView10);
+        TextView allEnvironment = findViewById(R.id.textView11);
+
+        String username;
+
+        TextView cusService = findViewById(R.id.textView1);
+        TextView price = findViewById(R.id.textView3);
+        TextView serviceQuality = findViewById(R.id.textView2);
+        TextView environment = findViewById(R.id.textView4);
+
+        String file = "salon.txt";
+        try {
+            FileInputStream fis = openFileInput(file);
+            InputStreamReader isr = new InputStreamReader(fis);
+            BufferedReader br = new BufferedReader(isr);
+            String line;
+            while ((line = br.readLine()) != null) {
+                String[] words = line.split(",\\s*");
+                if(words[0].equals(salonName)) {
+                    overall1.setText(words[5]);
+                    overall2.setText(words[5]);
+                    customerRating.setRating(Float.parseFloat(words[5]));
+                }
+            }
+            br.close();
+            isr.close();
+            fis.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
--- app/src/main/java/com/example/salonbookingapp/SalonDetail.java
+++ app/src/main/java/com/example/salonbookingapp/SalonDetail.java
@@ -1,6 +1,9 @@
 package com.example.salonbookingapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
@@ -19,19 +22,20 @@ import java.io.InputStreamReader;
 
 public class SalonDetail extends AppCompatActivity {
     String salonIntro;
+    String salonName;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_salon_detail);
-        String salonName = getIntent().getStringExtra("salonName");
+        salonName = getIntent().getStringExtra("salonName");
 
         TextView salon = findViewById(R.id.textView14);
         salon.setText(salonName);
         TextView intro = findViewById(R.id.textView16);
 
+
         String file = "salon.txt";
-        StringBuilder data = new StringBuilder();
         try {
             FileInputStream fis = openFileInput(file);
             InputStreamReader isr = new InputStreamReader(fis);
@@ -50,6 +54,12 @@ public class SalonDetail extends AppCompatActivity {
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    public void onClickReview(View view){
+        Intent intent = new Intent(this,Review.class);
+        intent.putExtra("salonName", salonName);
+        startActivity(intent);
 
     }
 }
