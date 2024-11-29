8c9afefa3b2f1c59dfc2d52d124fe36ac823a357
joyfuluj <joyumejiego@yahoo.com>
2024-11-29 12:47:19 -0800

    add hot picks function

--- app/src/main/java/com/example/salonbookingapp/HotPickFragment.java
+++ app/src/main/java/com/example/salonbookingapp/HotPickFragment.java
@@ -1,12 +1,23 @@
 package com.example.salonbookingapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -56,9 +67,143 @@ public class HotPickFragment extends Fragment {
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_hot_pick, container, false);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_hot_pick, container, false);
+        TextView test = view.findViewById(R.id.textView2);
+        LinearLayout mainLinearLayout = view.findViewById(R.id.firstLayout);
+        String file = "salon.txt";
+
+        String[] high = {"0", "0", "0"};
+        String[] salon = new String[3];
+
+        try (FileInputStream fis = requireContext().openFileInput(file);
+             InputStreamReader isr = new InputStreamReader(fis);
+             BufferedReader br = new BufferedReader(isr)) {
+
+            String line;
+            while ((line = br.readLine()) != null) {
+                String[] words3 = line.split(",\\s*");
+                String rating = words3[5];
+
+                if (!rating.equals("NA") && !rating.equals("rating")) {
+                    float ratingValue = Float.parseFloat(rating);
+
+                    for (int i = 0; i < 3; i++) {
+                        if (ratingValue > Float.parseFloat(high[i])) {
+                            for (int j = 2; j > i; j--) {
+                                high[j] = high[j - 1];
+                                salon[j] = salon[j - 1];
+                            }
+                            high[i] = rating;
+                            salon[i] = words3[0];
+                            break;
+                        }
+                    }
+                }
+            }
+
+
+        } catch (IOException | NumberFormatException e) {
+            e.printStackTrace();
+        }
+
+        try {
+
+            String line;
+            for (int i = 0; i < 3; i++) {
+                FileInputStream fis = requireContext().openFileInput(file);
+                InputStreamReader isr = new InputStreamReader(fis);
+                BufferedReader br = new BufferedReader(isr);
+                while ((line = br.readLine()) != null) {
+                    String[] words = line.split(",\\s*");
+                    String salonName = words[0];
+                    String[] servicesArray = words[3].split(";");
+                    String services = String.join(", ", servicesArray);
+
+                    if (salonName.equals(salon[i])) {
+                        LinearLayout horizontalLayout = new LinearLayout(requireContext());
+                        horizontalLayout.setOrientation(LinearLayout.HORIZONTAL);
+                        horizontalLayout.setLayoutParams(new LinearLayout.LayoutParams(
+                                LinearLayout.LayoutParams.MATCH_PARENT,
+                                LinearLayout.LayoutParams.WRAP_CONTENT
+                        ));
+
+                        ImageView imageView = new ImageView(requireContext());
+                        imageView.setImageResource(R.drawable.baseline_person_24);
+                        LinearLayout.LayoutParams imageParams = new LinearLayout.LayoutParams(120, 130);
+                        imageParams.setMargins(0, 0, 16, 0);
+                        imageView.setLayoutParams(imageParams);
+                        horizontalLayout.addView(imageView);
+
+                        LinearLayout textLayout = new LinearLayout(requireContext());
+                        textLayout.setOrientation(LinearLayout.VERTICAL);
+                        textLayout.setLayoutParams(new LinearLayout.LayoutParams(
+                                0,
+                                LinearLayout.LayoutParams.WRAP_CONTENT,
+                                1f
+                        ));
+
+                        TextView salonTextView = new TextView(requireContext());
+                        salonTextView.setText(salonName);
+                        salonTextView.setTextSize(28f);
+                        textLayout.addView(salonTextView);
+
+                        TextView serviceLabelTextView = new TextView(requireContext());
+                        serviceLabelTextView.setText("Service:");
+                        serviceLabelTextView.setTextSize(20f);
+                        textLayout.addView(serviceLabelTextView);
+
+                        TextView servicesTextView = new TextView(requireContext());
+                        servicesTextView.setText(services);
+                        servicesTextView.setTextSize(25f);
+                        textLayout.addView(servicesTextView);
+
+                        horizontalLayout.addView(textLayout);
+
+                        LinearLayout rightLayout = new LinearLayout(requireContext());
+                        rightLayout.setOrientation(LinearLayout.VERTICAL);
+                        rightLayout.setLayoutParams(new LinearLayout.LayoutParams(
+                                LinearLayout.LayoutParams.WRAP_CONTENT,
+                                LinearLayout.LayoutParams.WRAP_CONTENT
+                        ));
+
+                        TextView ratingTextView = new TextView(requireContext());
+                        ratingTextView.setText("⭐️" + high[i]);
+                        ratingTextView.setTextSize(30f);
+                        textLayout.addView(ratingTextView);
+
+                        Button detailButton = new Button(requireContext());
+                        detailButton.setText("detail");
+                        detailButton.setBackgroundColor(ContextCompat.getColor(requireContext(), R.color.light_green));
+
+                        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(
+                                LinearLayout.LayoutParams.WRAP_CONTENT,
+                                LinearLayout.LayoutParams.WRAP_CONTENT
+                        );
+                        params.setMargins(0, 80, 0, 0);
+                        detailButton.setLayoutParams(params);
+
+                        horizontalLayout.addView(detailButton);
+                        mainLinearLayout.addView(horizontalLayout);
+                        if(i != 2){
+                            View divider = new View(requireContext());
+                            divider.setLayoutParams(new LinearLayout.LayoutParams(
+                                    LinearLayout.LayoutParams.MATCH_PARENT,
+                                    2  // Height of the line (divider)
+                            ));
+                            divider.setBackgroundColor(ContextCompat.getColor(requireContext(), R.color.black));
+                            mainLinearLayout.addView(divider);
+                        }
+                    }
+                }
+                br.close();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return view;
     }
+
+
 }
